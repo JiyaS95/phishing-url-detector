d52781a915d750e805cb1c1fa1fab60c9afd49f7
JiyaS95 <jiya.shukla@mail.utoronto.ca>
2025-08-28 18:50:55 +0000

    Initialize URLChecker and URLResult classes

--- src/main/java/URLResult.java
+++ src/main/java/URLResult.java
@@ -5,5 +5,25 @@ public class URLResult {
     private String protocol;
     private String domain;
     private String path;
-    private List<String> warnings;
+    private List<String> warnings = new ArrayList<>();
+
+    public void setProtocol (String protocol) {
+        this.protocol = protocol;
+    }
+
+    public void setHost (String domain) {
+        this.domain = domain;
+    }
+
+    public void setPath (String path) {
+        this.path = path;
+    }
+
+    public void addWarning (String warning) {
+        warnings.add(warning);
+    }
+
+    public void print() {
+        System.out.println("hip-hip");
+    }
 }
