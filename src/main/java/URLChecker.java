04d5010793cb9582567c4185a3450da82cb083a8
JiyaS95 <jiya.shukla@mail.utoronto.ca>
2025-09-12 02:45:35 +0000

    Refactor URLChecker: split analysis into modular methods with dedicated checks and early blacklist exit

--- src/main/java/URLChecker.java
+++ src/main/java/URLChecker.java
@@ -4,103 +4,135 @@ import java.net.MalformedURLException;
 public class URLChecker {
     public static URLResult analyze(String url, Set<String>whitelistSet, Set<String>blacklistSet) {
         URLResult result = new URLResult();
+        URL urlObj;
         try {
-            URL urlObj = new URL(url);
-            result.setProtocol(urlObj.getProtocol()); //get the protocol from the URL and store it in the URLResult object
-            result.setHost(urlObj.getHost());
-            result.setPath(urlObj.getPath());
-            String host = urlObj.getHost();
+            urlObj = new URL(url);
             
-            int riskScore = 0; //Scoring system --> used to show risk level (low/medium/high)
-
-            //Check for blacklist/whitelist
-            Iterator<String> it = blacklistSet.iterator();
-
-            while (it.hasNext()) {
-                String badDomain = it.next();
-                if (host.endsWith(badDomain)) {
-                    result.addWarning("⚠️ Domain is blacklisted (HIGH RISK 🔴)");
-                    riskScore = 100;
-                    result.setRiskScore(riskScore);
-                    return result;
-                }
-            }
-            
-
-            it = whitelistSet.iterator();
-            while (it.hasNext()) {
-                String safeDomain = it.next();
-                if (host.endsWith(safeDomain)) {
-                    result.addWarning("✅ Domain is whitelisted (LOW RISK 🟢)");
-                }
-            }
-
-
-            //Check for extra subdomains
-            int dots = host.split("\\.").length;
-
-            if (dots > 3) {
-                result.addWarning("⚠️ Too many subdomains found (possible phishing)");
-                riskScore = riskScore + 15;
-            }
-            
-            
-            //Check for extra keywords
-            String[] keywords = {"login", "secure", "verify", "update", "account", "confirm", "payment", "banking", "transaction", "billing"}; //common or extra words that appear in the urls
-
-            int keywordRisk = 0;
-            for (int i = 0; i < keywords.length; i++) {
-                if (host.contains(keywords[i])) {
-                    result.addWarning("⚠️ Suspicious keyword detected: "+keywords[i]);
-                    keywordRisk = keywordRisk + 15;
-                }
-            }
-            if (keywordRisk > 30) {
-                riskScore = riskScore + 30;
-            }
-            else {
-                riskScore = riskScore + keywordRisk;
-            }
-
-            if (urlObj.getProtocol().equals("http")) {
-                result.addWarning("⚠️ Connection is not secure (uses http instead of https)");
-                riskScore = riskScore + 20;
-            }
-
-            //Check for long URLS
-            if (url.length() > 100) {
-                result.addWarning("⚠️ URL is very long (possible phishing)");
-                riskScore = riskScore + 10;
-            }
-            int digitCount = 0;
-            for (int i = 0; i < url.length(); i++) {
-                char c = url.charAt(i);
-                if (Character.isDigit(c)) {
-                    ++digitCount;
-                }
-            }
-            double digitPercent = ((double) digitCount / url.length()) * 100;
-            if (digitPercent >= 30) {
-                result.addWarning("⚠️ Too many digits detected (possible phishing)");
-                riskScore = riskScore + 20;
-            }
-            
-            //Check IP Address
-            if (host.matches("^\\d+\\.\\d+\\.\\d+\\.\\d+$") || host.contains(":")) {
-                result.addWarning("⚠️ Domain is an IP Address (possible phishing)");
-                riskScore = riskScore + 40;
-            }
-
-            
-
-
-
-
         }
         catch (MalformedURLException e) {
             System.out.println("❌ Invalid URL format");
+            return new URLResult(); //return empty result if URL is bad
         }
+
+        String host = urlObj.getHost();
+        result.setProtocol(urlObj.getProtocol()); //get the protocol from the URL and store it in the URLResult object
+        result.setHost(urlObj.getHost());
+        result.setPath(urlObj.getPath());
+            
+        int riskScore = 0; //Scoring system --> used to show risk level (low/medium/high)
+        
+        //Call each method and store corresponding risk score
+        riskScore = riskScore + checkBlacklist(result, host, blacklistSet);
+        if (riskScore == 100) {
+            result.setRiskScore(riskScore);
+            return result;
+        }
+        riskScore = riskScore + checkWhitelist(result, host, whitelistSet);
+        riskScore = riskScore + checkSubdomains(result, host);
+        riskScore = riskScore + checkKeywords(result, host);
+        riskScore = riskScore + checkProtocol(result, urlObj);
+        riskScore = riskScore + checkDigitsAndLength(result, url);
+        riskScore = riskScore + checkIPAddress(result, host);
         result.setRiskScore(riskScore);
         return result;
+    }
+
+    public static int checkBlacklist (URLResult result, String host, Set<String> blacklistSet) {
+        int riskScore = 0;
+        Iterator<String> it = blacklistSet.iterator();
+
+        while (it.hasNext()) {
+            String badDomain = it.next();
+            if (host.endsWith(badDomain)) {
+                result.addWarning("⚠️ Domain is blacklisted (HIGH RISK 🔴)");
+                return 100;
+            }
+        }
+        
+        
+        return riskScore;
+    }
+    public static int checkWhitelist (URLResult result, String host, Set<String> whitelistSet) {
+        int riskScore = 0;
+        Iterator<String> it = whitelistSet.iterator();
+        while (it.hasNext()) {
+            String safeDomain = it.next();
+            if (host.endsWith(safeDomain)) {
+                result.addWarning("✅ Domain is whitelisted (LOW RISK 🟢)");
+            }
+        }
+        return riskScore;
+    }
+
+    public static int checkSubdomains (URLResult result, String host) {
+        int riskScore = 0;
+        int dots = host.split("\\.").length;
+
+        if (dots > 3) {
+            result.addWarning("⚠️ Too many subdomains found (possible phishing)");
+            riskScore = 15;
+        }
+        return riskScore;
+    }
+
+    public static int checkKeywords (URLResult result, String host) {
+        int riskScore = 0;
+        //Check for extra keywords
+        String[] keywords = {"login", "secure", "verify", "update", "account", "confirm", "payment", "banking", "transaction", "billing"}; //common or extra words that appear in the urls
+
+        int keywordRisk = 0;
+        for (int i = 0; i < keywords.length; i++) {
+            if (host.contains(keywords[i])) {
+                result.addWarning("⚠️ Suspicious keyword detected: "+keywords[i]);
+                keywordRisk = keywordRisk + 15;
+            }
+        }
+        if (keywordRisk > 30) {
+            riskScore = 30;
+        }
+        else {
+            riskScore = keywordRisk;
+        }
+
+        return riskScore;
+    }
+
+    public static int checkProtocol (URLResult result, URL urlObj) {
+        int riskScore = 0;
+        if (urlObj.getProtocol().equals("http")) {
+            result.addWarning("⚠️ Connection is not secure (uses http instead of https)");
+            riskScore = 20;
+        }
+        return riskScore;
+    }
+
+    public static int checkDigitsAndLength (URLResult result, String url) {
+        int riskScore = 0;
+        if (url.length() > 100) {
+            result.addWarning("⚠️ URL is very long (possible phishing)");
+            riskScore = 10;
+        }
+        int digitCount = 0;
+        for (int i = 0; i < url.length(); i++) {
+            char c = url.charAt(i);
+            if (Character.isDigit(c)) {
+                ++digitCount;
+            }
+        }
+        double digitPercent = ((double) digitCount / url.length()) * 100;
+        if (digitPercent >= 30) {
+            result.addWarning("⚠️ Too many digits detected (possible phishing)");
+            riskScore = 20;
+        }
+        return riskScore;
+    }
+
+    public static int checkIPAddress (URLResult result, String host) {
+        int riskScore = 0;
+        if (host.matches("^\\d+\\.\\d+\\.\\d+\\.\\d+$") || host.contains(":")) {
+            result.addWarning("⚠️ Domain is an IP Address (possible phishing)");
+            riskScore = 40;
+        }
+        return riskScore;
     }
 }
