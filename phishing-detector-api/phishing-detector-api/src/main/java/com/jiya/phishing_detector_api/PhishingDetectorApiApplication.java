57576f591b98fd8036624a2006d1c125cd4383ce
JiyaS95 <jiya.shukla@mail.utoronto.ca>
2025-12-22 00:18:57 +0000

    Add initial URL analysis API endpoint

---
+++ phishing-detector-api/phishing-detector-api/src/main/java/com/jiya/phishing_detector_api/PhishingDetectorApiApplication.java
@@ -0,0 +1,13 @@
+package com.jiya.phishing_detector_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingDetectorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhishingDetectorApiApplication.class, args);
+	}
+
+}
